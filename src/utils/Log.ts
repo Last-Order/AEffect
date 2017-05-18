2f5fda4d1e34e0e0164372635da859c850a2f43d
Ted Zyzsdy <zyzsdy@gmail.com>
2017-05-18 14:38:34 +0800

    没啥意义，不要了。

--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -1,10 +1,6 @@
 export default class Log{
     static error(name, message){
         console.error(`[AEffect] [${new Date().toString()}] [ERROR] Error Name: ${name}; Message: ${message}`);
-        let error = new Error();
-        error.name = name;
-        error.message = message;
-        //throw error;
     }
     static warning(message){
         console.warn(`[AEffect] [${new Date().toString()}] [WARN] ${message}`);
